5428291aabf3ecdd0b3bd5e34ae3fbc263c2d366
Kilian <31303@ma-web.nl>
2021-10-15 10:23:00 +0200

    Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+    }
+    public class Rectangle
+    {
+        public string bird;
+        private int x;
+        private int y;
+        private int width;
+        private int height;
+        private bool Square = true;
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+
+        }
+
+        public Rectangle(int x, int y, int size)
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+
+        }
+
+        public Rectangle(int Size)
+        {
+
+        }
+
+        public void MoveTo(int x, int y)
+        {
+
+        }
+
+        public void IsSquare()
+        {
+            if ()
+            {
+                Console.WriteLine("Vierkant");
+            }
+        }
+        public void Set(int x, int y, int width, int height)
+        {
+
+        }
+    }
+}
---
+++ Rooms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Rooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    public class Room
+    {
+        
+       public void Kamer(int x, int y)
+        {
+            int[][] coordinaten = new int[10][];
+            for (int i = 2; i < coordinaten.Length; i++)
+            {
+                coordinaten[i] = new int[4];
+
+                for (int coordI = 2; coordI < coordinaten[i].Length; coordI++)
+                {
+                    coordinaten[i][coordI] = 2;
+                }
+            }
+            
+        }
+    }
+}
