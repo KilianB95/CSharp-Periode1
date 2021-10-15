b872fc53d60f256b68b69971c5c1bee4c2ba1c56
Kilian <31303@ma-web.nl>
2021-10-15 10:24:01 +0200

    Commit two

---
+++ Inheritance2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Inheritance2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+    
+    abstract class onPickup
+    {
+        public virtual void PickUp()
+        {
+
+        }
+        void MouseClick(float x, float y)
+        {
+
+        }
+    }
+    
+    abstract class Equipment:onPickup
+    {
+        public virtual void Equip()
+        {
+
+        }
+    }
+    class Armor:Equipment
+    {
+        public override void Equip()
+        {
+            base.Equip();
+            Console.WriteLine("Draconic Armor");
+        }
+    }
+    class Weapon:Equipment
+    {
+        public override void Equip()
+        {
+            base.Equip();
+            Console.WriteLine("Deadric GreatSword");
+        }
+    }
+    class HealPotion : onPickup
+    {
+        public override void PickUp()
+        {
+            base.PickUp();
+            Console.WriteLine("Greater Heal Potion");
+        }
+        class ManaPotion : onPickup
+        {
+            public override void PickUp()
+            {
+                base.PickUp();
+                Console.WriteLine("Greater Mana Potion");
+            }
+        }
+
+    }
+    
+}
---
+++ SpriteInheritance/SpriteInheritance/Bobby.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Eindopdracht
+{
+    class Bobby : Sprite
+    {
+        public Bobby(RectangleF screenPosition)
+        {
+            this.screenPosition = screenPosition;
+            imageFrame = new Rectangle(95, 286, 16, 16);
+        }
+    }
+}
