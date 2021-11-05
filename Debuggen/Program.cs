a6dfa26a35cac2bfa86a523a79ef16313a5516c7
Kilian <31303@ma-web.nl>
2021-11-05 09:40:07 +0100

    Added Debug

---
+++ Debuggen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Debuggen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string naam = "Kilian";
+
+            Console.WriteLine(naam);
+        }
+    }
+}
